0cf3254ebfa37fd5153c5028096a942d7e177f42
Félix Mielcarek <felix.mielcarek@etu.uca.fr>
2024-05-15 09:39:23 +0200

    Adding test view with stub data

---
+++ Sources/test/contact_view_test/home-page.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:test_flutter_vue/data/factories/conversations_factory.dart';
+import 'package:test_flutter_vue/model/conversation_model.dart';
+import 'package:test_flutter_vue/services/interfaces/conversation_service.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  ConversationModel? conversation;
+
+  @override
+  void initState() {
+    super.initState();
+    conversation = ConversationsFactory.DTOToPOCO(getIt
+        .get<ConversationService>()
+        .getConversations()
+        .singleWhere((conv) => conv.contactId == '1001'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Container(
+            margin: const EdgeInsets.only(top: 50.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    const Expanded(
+                      child: Text('Name', textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                        child: conversation != null
+                            ? Text(conversation!.contact.name,
+                                textAlign: TextAlign.center)
+                            : const Text('undefined',
+                                textAlign: TextAlign.center))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    const Expanded(
+                      child:
+                          Text('Profile picture', textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      child: Image.memory(
+                        base64Decode(conversation!.contact.profilePictureBase64!
+                            .split(',')
+                            .last),
+                        fit:
+                            BoxFit.cover, // adjust this according to your needs
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    const Expanded(
+                      child: Text('Messages', textAlign: TextAlign.center),
+                    ),
+                    Expanded(
+                      child: Text(conversation!.messages.first.content,
+                          textAlign: TextAlign.center),
+                    )
+                  ],
+                ),
+              ],
+            )));
+  }
+}
